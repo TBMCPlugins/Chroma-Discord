73629cbd64485f35325ceb070915767471d6aaf3
NorbiPeti <szatmari.norbert.peter@gmail.com>
2016-10-24 20:09:58 +0200

    Fixed another import

--- src/main/java/buttondevteam/discordplugin/EventExceptionDiscordSender.java
+++ src/main/java/buttondevteam/discordplugin/EventExceptionDiscordSender.java
@@ -1,8 +1,9 @@
 package buttondevteam.discordplugin;
 
 import org.bukkit.event.Event;
+import buttondevteam.lib.EventExceptionHandler;
 
-public class EventExceptionDiscordSender implements EventExceptionHandler {
+public class EventExceptionDiscordSender extends EventExceptionHandler {
 	@Override
 	public boolean handle(Throwable ex, Event event) {
 		TBMCDiscordAPI.SendException(ex, "An error occured while executing " + event.getEventName() + "!");
