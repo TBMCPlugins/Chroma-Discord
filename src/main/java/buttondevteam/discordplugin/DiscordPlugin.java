81f531fa6cbb83a131ae67ea444a3bb6dafa4861
alisolarflare <alisunderstorm@gmail.com>
2016-11-23 11:06:44 -0500

    Deleted serverissues and debug channel

--- src/main/java/buttondevteam/discordplugin/DiscordPlugin.java
+++ src/main/java/buttondevteam/discordplugin/DiscordPlugin.java
@@ -74,8 +74,6 @@ public class DiscordPlugin extends JavaPlugin implements IListener<ReadyEvent> {
 	public static IChannel botchannel;
 	public static IChannel annchannel;
 	public static IChannel genchannel;
-	public static IChannel issuechannel;
-	public static IChannel debugchannel;
 	public static IChannel chatchannel;
 
 	public static boolean Test = true;
@@ -94,16 +92,12 @@ public class DiscordPlugin extends JavaPlugin implements IListener<ReadyEvent> {
 				botchannel = mainServer.getChannelByID("209720707188260864"); // bot
 				annchannel = mainServer.getChannelByID("126795071927353344"); // announcements
 				genchannel = mainServer.getChannelByID("125813020357165056"); // general
-				issuechannel = devServer.getChannelByID("219643416496046081"); // server-issues
-				debugchannel = devServer.getChannelByID("250332016199860224"); // debug-channel
 				chatchannel = mainServer.getChannelByID("249663564057411596"); // minecraft_chat
 				dc.changeStatus(Status.game("on TBMC"));
 			} else {
 				botchannel = devServer.getChannelByID("239519012529111040"); // bottest
 				annchannel = devServer.getChannelByID("239519012529111040"); // bottest
 				genchannel = devServer.getChannelByID("239519012529111040"); // bottest
-				issuechannel = devServer.getChannelByID("239519012529111040"); // bottest
-				debugchannel = devServer.getChannelByID("239519012529111040"); //bottest
 				chatchannel = devServer.getChannelByID("248185455508455424"); // minecraft_chat_test
 				dc.changeStatus(Status.game("testing"));
 			}
