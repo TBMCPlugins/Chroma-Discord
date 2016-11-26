cadd04ccd10b0ca83c907ebd04060ca9505c9c0b
NorbiPeti <szatmari.norbert.peter@gmail.com>
2016-11-26 15:36:46 +0100

    Removed server shutdown message

--- src/main/java/buttondevteam/discordplugin/listeners/MCListener.java
+++ src/main/java/buttondevteam/discordplugin/listeners/MCListener.java
@@ -16,23 +16,6 @@ import sx.blah.discord.handle.obj.Status.StatusType;
 
 public class MCListener implements Listener {
 	@EventHandler
-	public void onServerCommandPreprocess(ServerCommandEvent e) {
-		if (!DiscordPlugin.dc.isReady())
-			return;
-		try {
-			if (e.getCommand().equalsIgnoreCase("stop"))
-				DiscordPlugin.sendMessageToChannel(DiscordPlugin.botchannel, "Minecraft server shutting down!");
-			/*
-			 * else if (e.getCommand().equalsIgnoreCase("restart"))
-			 * DiscordPlugin.sendMessageToChannel(DiscordPlugin.botchannel,
-			 * "Minecraft server restarting");
-			 */
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
-
-	@EventHandler
 	public void onPlayerJoin(TBMCPlayerJoinEvent e) {
 		final Player p = Bukkit.getPlayer(e.GetPlayer().getUuid());
 		if (ConnectCommand.WaitingToConnect.containsKey(e.GetPlayer().getPlayerName())) {
