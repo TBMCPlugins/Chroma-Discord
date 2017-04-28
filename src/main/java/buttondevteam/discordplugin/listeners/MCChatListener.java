c23d9aee09cb6a24b83e62db34f682b919169b2a
NorbiPeti <szatmari.norbert.peter@gmail.com>
2017-04-28 22:41:23 +0200

    Fixed chat msg loss <@187257785199624194>
    Probably
    
    Also fixed exception filtering

--- src/main/java/buttondevteam/discordplugin/listeners/MCChatListener.java
+++ src/main/java/buttondevteam/discordplugin/listeners/MCChatListener.java
@@ -28,7 +28,9 @@ public class MCChatListener implements Listener, IListener<MessageReceivedEvent>
 			return;
 		if (e.getSender() instanceof DiscordSender || e.getSender() instanceof DiscordPlayerSender)
 			return;
-		if (e.getChannel().equals(Channel.GlobalChat)) {
+		if (!e.getChannel().equals(Channel.GlobalChat))
+			return;
+		synchronized (this) {
 			final String authorPlayer = DiscordPlugin.sanitizeString(e.getSender() instanceof Player //
 					? ((Player) e.getSender()).getDisplayName() //
 					: e.getSender().getName());
@@ -83,76 +85,78 @@ public class MCChatListener implements Listener, IListener<MessageReceivedEvent>
 		if (CommandListener.runCommand(event.getMessage(), true))
 			return;
 		String dmessage = event.getMessage().getContent();
-		try {
-			DiscordPlayer dp = ChromaGamerBase.getUser(author.getID(), DiscordPlayer.class);
-			final DiscordSenderBase dsender;
-			Player mcp = null; // Offline players can't really run commands
-			final String cid;
-			if ((cid = dp.getConnectedID(TBMCPlayer.class)) != null // Connected?
-					&& (mcp = Bukkit.getPlayer(cid)) != null) { // Execute as ingame player, if online
-				if (!ConnectedSenders.containsKey(author.getID()))
-					ConnectedSenders.put(author.getID(),
-							new DiscordPlayerSender(author, event.getMessage().getChannel(), mcp));
-				dsender = ConnectedSenders.get(author.getID());
-			} else {
-				TBMCPlayer p = dp.getAs(TBMCPlayer.class);
-				if (!UnconnectedSenders.containsKey(author.getID()))
-					UnconnectedSenders.put(author.getID(), new DiscordSender(author, event.getMessage().getChannel(),
-							p == null ? null : p.PlayerName().get())); // Display the playername, if found
-				dsender = UnconnectedSenders.get(author.getID());
-			}
-
-			for (IUser u : event.getMessage().getMentions()) {
-				dmessage = dmessage.replace(u.mention(false), "@" + u.getName()); // TODO: IG Formatting
-				final Optional<String> nick = u.getNicknameForGuild(DiscordPlugin.mainServer);
-				dmessage = dmessage.replace(u.mention(true), "@" + (nick.isPresent() ? nick.get() : u.getName()));
-			}
-
-			if (dmessage.startsWith("/")) {
-				final String cmd = dmessage.substring(1).toLowerCase();
-				if (mcp == null
-						&& !Arrays.stream(UnconnectedCmds).anyMatch(s -> cmd.equals(s) || cmd.startsWith(s + " "))) {
-					// Command not whitelisted
-					DiscordPlugin.sendMessageToChannel(event.getMessage().getChannel(), // TODO
-							"Sorry, you need to be online on the server and have your accounts connected, you can only access these commands:\n"
-									+ Arrays.stream(UnconnectedCmds).map(uc -> "/" + uc)
-											.collect(Collectors.joining(", "))
-									+ "\nTo connect your accounts, use @ChromaBot connect in "
-									+ DiscordPlugin.botchannel.mention());
-					return;
+		synchronized (this) {
+			try {
+				DiscordPlayer dp = ChromaGamerBase.getUser(author.getID(), DiscordPlayer.class);
+				final DiscordSenderBase dsender;
+				Player mcp = null; // Offline players can't really run commands
+				final String cid;
+				if ((cid = dp.getConnectedID(TBMCPlayer.class)) != null // Connected?
+						&& (mcp = Bukkit.getPlayer(cid)) != null) { // Execute as ingame player, if online
+					if (!ConnectedSenders.containsKey(author.getID()))
+						ConnectedSenders.put(author.getID(),
+								new DiscordPlayerSender(author, event.getMessage().getChannel(), mcp));
+					dsender = ConnectedSenders.get(author.getID());
+				} else {
+					TBMCPlayer p = dp.getAs(TBMCPlayer.class);
+					if (!UnconnectedSenders.containsKey(author.getID()))
+						UnconnectedSenders.put(author.getID(), new DiscordSender(author,
+								event.getMessage().getChannel(), p == null ? null : p.PlayerName().get())); // Display the playername, if found
+					dsender = UnconnectedSenders.get(author.getID());
 				}
-				if (lastlist > 5) {
-					ListC = 0;
-					lastlist = 0;
+
+				for (IUser u : event.getMessage().getMentions()) {
+					dmessage = dmessage.replace(u.mention(false), "@" + u.getName()); // TODO: IG Formatting
+					final Optional<String> nick = u.getNicknameForGuild(DiscordPlugin.mainServer);
+					dmessage = dmessage.replace(u.mention(true), "@" + (nick.isPresent() ? nick.get() : u.getName()));
 				}
-				if (cmd.equals("list") && Bukkit.getOnlinePlayers().size() == lastlistp && ListC++ > 2) // Lowered already
-				{
-					dsender.sendMessage("Stop it. You know the answer.");
-					lastlist = 0;
-				} else
-					Bukkit.dispatchCommand(dsender, cmd);
-				lastlistp = (short) Bukkit.getOnlinePlayers().size();
-				if (!event.getMessage().isDeleted())
-					event.getMessage().delete();
-			} else {
-				TBMCChatAPI.SendChatMessage(Channel.GlobalChat, dsender,
-						dmessage + (event.getMessage().getAttachments().size() > 0 ? "\n" + event.getMessage()
-								.getAttachments().stream().map(a -> a.getUrl()).collect(Collectors.joining("\n"))
-								: ""));
-				event.getMessage().getChannel().getMessages().stream().forEach(m -> {
-					try {
-						final IReaction reaction = m.getReactionByName(DiscordPlugin.DELIVERED_REACTION);
-						if (reaction != null)
-							DiscordPlugin.perform(() -> m.removeReaction(reaction));
-					} catch (Exception e) {
-						TBMCCoreAPI.SendException("An error occured while removing reactions from chat!", e);
+
+				if (dmessage.startsWith("/")) {
+					final String cmd = dmessage.substring(1).toLowerCase();
+					if (mcp == null && !Arrays.stream(UnconnectedCmds)
+							.anyMatch(s -> cmd.equals(s) || cmd.startsWith(s + " "))) {
+						// Command not whitelisted
+						DiscordPlugin.sendMessageToChannel(event.getMessage().getChannel(), // TODO
+								"Sorry, you need to be online on the server and have your accounts connected, you can only access these commands:\n"
+										+ Arrays.stream(UnconnectedCmds).map(uc -> "/" + uc)
+												.collect(Collectors.joining(", "))
+										+ "\nTo connect your accounts, use @ChromaBot connect in "
+										+ DiscordPlugin.botchannel.mention());
+						return;
 					}
-				});
-				DiscordPlugin.perform(() -> event.getMessage().addReaction(DiscordPlugin.DELIVERED_REACTION));
+					if (lastlist > 5) {
+						ListC = 0;
+						lastlist = 0;
+					}
+					if (cmd.equals("list") && Bukkit.getOnlinePlayers().size() == lastlistp && ListC++ > 2) // Lowered already
+					{
+						dsender.sendMessage("Stop it. You know the answer.");
+						lastlist = 0;
+					} else
+						Bukkit.dispatchCommand(dsender, cmd);
+					lastlistp = (short) Bukkit.getOnlinePlayers().size();
+					if (!event.getMessage().isDeleted())
+						event.getMessage().delete();
+				} else {
+					TBMCChatAPI.SendChatMessage(Channel.GlobalChat, dsender,
+							dmessage + (event.getMessage().getAttachments().size() > 0 ? "\n" + event.getMessage()
+									.getAttachments().stream().map(a -> a.getUrl()).collect(Collectors.joining("\n"))
+									: ""));
+					event.getMessage().getChannel().getMessages().stream().forEach(m -> {
+						try {
+							final IReaction reaction = m.getReactionByName(DiscordPlugin.DELIVERED_REACTION);
+							if (reaction != null)
+								DiscordPlugin.perform(() -> m.removeReaction(reaction));
+						} catch (Exception e) {
+							TBMCCoreAPI.SendException("An error occured while removing reactions from chat!", e);
+						}
+					});
+					DiscordPlugin.perform(() -> event.getMessage().addReaction(DiscordPlugin.DELIVERED_REACTION));
+				}
+			} catch (Exception e) {
+				TBMCCoreAPI.SendException("An error occured while handling message \"" + dmessage + "\"!", e);
+				return;
 			}
-		} catch (Exception e) {
-			TBMCCoreAPI.SendException("An error occured while handling message \"" + dmessage + "\"!", e);
-			return;
 		}
 	}
 }
