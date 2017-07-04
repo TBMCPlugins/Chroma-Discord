9d0525571b4c6a0bf6ac2eab9cff495c6419847f
NorbiPeti <szatmari.norbert.peter@gmail.com>
2017-07-04 03:14:10 +0200

    Lombok still doesn't work compile-time

--- src/main/java/buttondevteam/discordplugin/DiscordPlayerSender.java
+++ src/main/java/buttondevteam/discordplugin/DiscordPlayerSender.java
@@ -1,6 +1,15 @@
 package buttondevteam.discordplugin;
 
-import org.bukkit.entity.*;
+import java.util.Set;
+
+import org.bukkit.Server;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionAttachment;
+import org.bukkit.permissions.PermissionAttachmentInfo;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.projectiles.ProjectileSource;
 import org.bukkit.util.Vector;
 
@@ -9,7 +18,10 @@ import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IUser;
 
 public class DiscordPlayerSender extends DiscordSenderBase implements Player {
-	private @Delegate(excludes = ProjectileSource.class) Player player;
+
+	protected @Delegate(excludes = ProjectileSource.class) Player player;
+	// protected @Delegate(excludes = { ProjectileSource.class, Permissible.class }) Player player;
+	// protected @Delegate(excludes = { ProjectileSource.class, CommandSender.class }) Player player;
 
 	public DiscordPlayerSender(IUser user, IChannel channel, Player player) {
 		super(user, channel);
