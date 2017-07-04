69599cdd20aed520707526e0c7efe53e3752cd13
NorbiPeti <szatmari.norbert.peter@gmail.com>
2017-07-04 21:48:23 +0200

    Added one line that changes everything
    Hopefully in a good way

--- src/main/java/buttondevteam/discordplugin/listeners/MCChatListener.java
+++ src/main/java/buttondevteam/discordplugin/listeners/MCChatListener.java
@@ -52,6 +52,7 @@ public class MCChatListener implements Listener, IListener<MessageReceivedEvent>
 					lastmsgdata.message = DiscordPlugin.sendMessageToChannel(lastmsgdata.channel, msg, embedObject);
 					lastmsgdata.time = nanoTime;
 					lastmsgdata.mcchannel = e.getChannel();
+					lastmsgdata.content = "";
 				} else
 					try {
 						lastmsgdata.content = embedObject.description = lastmsgdata.content + "\n"
